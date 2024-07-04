bb97cc66cfd9d4cb9f616cf6d5319b7232f3d8b5
ViswajethTSR <viswa.dev.in@gmail.com>
2024-07-04 20:13:32 +0530

    Finished

--- src/controllers/mongo_connect.ts
+++ src/controllers/mongo_connect.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import '../models/user_model';
 
-const mongodb_uri: string | any = "mongodb+srv://viswajeth:viswajeth123@cluster0.1vyfusy.mongodb.net/task?retryWrites=true&w=majority&appName=Cluster0";
+const mongodb_uri: string | any = process.env.MONGODB_URI;
 mongoose.connect(mongodb_uri).then(() => {
     console.log('MongoDB connection established.');
 })
