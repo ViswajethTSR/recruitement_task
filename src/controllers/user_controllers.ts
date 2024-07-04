ea0426cada0550d45a84f14a8fef39ebefa0311d
ViswajethTSR <viswa.dev.in@gmail.com>
2024-07-04 19:52:22 +0530

    paging is completed

--- src/controllers/user_controllers.ts
+++ src/controllers/user_controllers.ts
@@ -1,10 +1,10 @@
 import { Router, Request, Response } from 'express';
-import { IUser, UserModel } from '../models/user_model';
+import { UserModel } from '../models/user_model';
 
 export const createUser = async (req: Request, res: Response) => {
     try {
         const { name, age, ph_no, city } = req.body;
-        const user = new UserModel({ name, age, ph_no,city});
+        const user = new UserModel({ name, age, ph_no, city });
         await user.save();
         res.status(201).json(user);
     } catch (e) {
@@ -15,7 +15,7 @@ export const createUser = async (req: Request, res: Response) => {
 // Get users with filters, sorting, and pagination
 export const fetchUser = async (req: Request, res: Response) => {
     try {
-        const { name, sort, order = 'asc', page = 1, limit = 10 } = req.query;
+        const { name, sort, order = 'asc', page, limit } = req.query;
 
         const query = name ? { name: new RegExp(name as string, 'i') } : {};
 
@@ -23,12 +23,13 @@ export const fetchUser = async (req: Request, res: Response) => {
 
         const users = await UserModel.find(query)
             .sort({ age: sortOrder })
-            .skip((Number(page) - 1) * Number(limit))
+            .skip((Number(page === '0' ? 1 : page) - 1) * Number(limit))
             .limit(Number(limit));
 
         res.json(users);
     } catch (e) {
         res.status(500).json({ message: e });
+        console.log(e);
     }
 };
 
